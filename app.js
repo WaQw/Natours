68abeb38761967ce2d3c7e66a4f7789269166c37
WaQw <47028829+WaQw@users.noreply.github.com>
2024-01-14 22:28:49 -0500

    add methods

--- app.js
+++ app.js
@@ -56,6 +56,36 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+// update part of a tour (demo)
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour: '<Updated tour here...>' },
+  });
+});
+
+// delete a tour (demo)
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
