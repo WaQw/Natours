e31fe63e3b58c9dd5dbd2dc98b036327266d34fb
WaQw <47028829+WaQw@users.noreply.github.com>
2024-01-19 23:15:50 -0500

    add some configurations

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
