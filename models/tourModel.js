4f20af26df926fe9d696c9a0359bc4910cb20279
WaQw <47028829+WaQw@users.noreply.github.com>
2024-02-09 23:47:47 -0500

    add model fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,27 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'], // displayed error when missing
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulity'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   rating: {
     type: Number,
@@ -15,6 +36,26 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // create model
