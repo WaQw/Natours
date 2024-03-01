a1fc72a81a4205a990507ba6b3a2a47d31be6fa8
WaQw <47028829+WaQw@users.noreply.github.com>
2024-03-01 16:51:28 -0500

    handle db errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,65 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErrorProd = (error, res) => {
+  // Operational, trusted error: send msg to client
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+
+    // Programming or other unknown eror: don't leak err details
+  } else {
+    console.error('ERROR 🔥', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let err = { ...error };
+    if (error.name === 'CastError') {
+      err = handleCastErrorDB(err);
+    }
+    if (error.code === 11000) {
+      err = handleDuplicateFieldsDB(err);
+    }
+    if (error.name === 'ValidationError') {
+      err = handleValidationErrorDB(err);
+    }
+    sendErrorProd(err, res);
+  }
 };
